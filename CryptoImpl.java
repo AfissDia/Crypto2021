ffb2491e26998e043070f49757fa3af8d281708b
Khaly17 <diengkhaly17@gmail.com>
2021-06-04 19:43:16 +0000

    sauvegarder la cle

--- CryptoImpl.java
+++ CryptoImpl.java
@@ -1,3 +1,6 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
@@ -19,8 +22,16 @@ public class CryptoImpl implements Icrypto{
 
     @Override
     public String saveKey(SecretKey key, String filePath) {
-       
-        
+       try {
+        FileOutputStream fos = new FileOutputStream(filePath);
+        ObjectOutputStream obj = new ObjectOutputStream(fos);
+        obj.writeObject(key);
+        obj.close();
+        fos.close();
+        return filePath;
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
         return null;
     }
 
--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
+import java.util.Base64;
+
 import javax.crypto.SecretKey;
+import javax.swing.JFileChooser;
 
 public class Main {
     public static void main(String[] args){
         CryptoImpl crypto = new CryptoImpl();
         SecretKey key = crypto.genKey("AES",128);
-        System.out.println(key.getAlgorithm());
-        System.out.println(key.getEncoded().length*8);
-    }
-    
+        JFileChooser jfc = new JFileChooser();
+        int option = jfc.showSaveDialog(jfc);
+        if(JFileChooser.APPROVE_OPTION==option){
+            String filePath = jfc.getSelectedFile().getAbsolutePath();
+            crypto.saveKey(key, filePath);
+        }
+        
+        crypto.saveKey(key, "mykey.khd");
+        // String encoding = Base64.getEncoder().encodeToString(key.getEncoded());
+        // System.out.println(encoding);
+    } 
 }
